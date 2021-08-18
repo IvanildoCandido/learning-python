bba2245398b02951a1f3e1ddb81ac3999fe9c255
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-08-18 20:40:01 -0300

    Finish showType and sum

---
+++ showType.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia algo e mostre o seu tipo primitivo
+# e todas as informações possíveis sobre ele
+a = input("Digite algo: ")
+print("O tipo primitivo de {} é {}! ".format(a, type(a)))
+print("Só tem espaços? ", a.isspace())
+print("É um número? ", a.isnumeric())
+print("É alfabetico? ", a.isalpha())
+print("É alfanumérico? ", a.isalnum())
+print("Está em maiúsculas? ", a.isupper())
+print("Está em minúsculas? ", a.islower())
+print("Está capitalizada? ", a.istitle())
---
+++ sum.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba 2 numeros e realize a soma
+num1 = input("Digite o primeiro numero: ")
+num2 = input("Digite o segundo numero: ")
+sum = int(num1) + int(num2)
+print("A soma de {} com {} é {}!".format(num1, num2, sum))
