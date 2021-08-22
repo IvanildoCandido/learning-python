a5a252e951519a6a7123f6b466d577e4a8f39c9b
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-08-21 21:23:55 -0300

    add calc_hypotenuse program

---
+++ calc_hypotenuse.py
@@ -0,0 +1,15 @@
+"""
+    Faça um programa que leia o comprimento do cateto oposto
+    e do cateto adjacente de um triangulo retângulo e calcule
+    e mostre o comprimento da hipotenusa.
+"""
+from math import pow, sqrt
+
+opposite_leg = float(input("Digite o comprimento do cateto oposto: "))
+adjacent_leg = float(input("Digite o comprimento do cateto adjacente: "))
+hypotenuse = pow(opposite_leg, 2) + pow(adjacent_leg, 2)
+print(
+    "O comprimento da hipotenusa do triangulo é {:.2f}".format(
+        sqrt(hypotenuse)
+    )
+)
