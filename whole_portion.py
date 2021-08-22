2df7fc32901854052ca40b3e4c0eb5b86e29834a
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-08-21 21:16:20 -0300

    add whole_portion program

--- whole_portion.py
+++ whole_portion.py
@@ -2,3 +2,7 @@
     Crie um programa que leia um número real qualquer
     e mostre na tela a sua porção inteira.
 """
+from math import floor
+
+number = float(input("Digite um número real qualquer! "))
+print("A parte inteira do número {} é {}.".format(number, floor(number)))
